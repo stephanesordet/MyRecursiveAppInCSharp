7a4e4b9d7eb1600ae1d7fd9a04192ea20d0093e6
SORDET Stephane <Stephane.SORDET@cpnv.ch>
2019-03-14 11:52:59 +0100

    Ajout Factoriel

--- MyRecursiveAppInCSharp/Form1.cs
+++ MyRecursiveAppInCSharp/Form1.cs
@@ -22,9 +22,40 @@ namespace MyRecursiveAppInCSharp
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+       
 
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            int resultat = 0;
+            lblResult.Text = "Resultat";
+
+            if (int.TryParse(txtInput.Text, out resultat))
+            {
+                if (resultat >= 1)
+                {
+                    lblResult.Text += " : " + Factoriel(resultat).ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Veuillez entrer une valeur supérieure ou égale à 1 !");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez entrer un nombre !");
+            }
+        }
+
+        private int Factoriel(int nb_init)
+        {
+            if (nb_init == 1)
+            {
+                return nb_init;
+            }
+            else
+            {
+                return nb_init * Factoriel(nb_init - 1);
+            }
         }
     }
 }
